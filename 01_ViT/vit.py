003b49095ce43b99b67b69397e8fe57cd154cb23
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-07-31 18:02:30 +0530

    settings

--- 01_ViT/vit.py
+++ 01_ViT/vit.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 
 import utils
 
+class PatchEmbeddings(t.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.patch_embd = t.nn.Conv2d(in_channels=1)
