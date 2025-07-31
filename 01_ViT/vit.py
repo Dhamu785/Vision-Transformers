99fc8bad8591a6072e1a5c28c0c9701a4ee6dcef
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-07-31 18:42:22 +0530

    patch embedding

--- 01_ViT/vit.py
+++ 01_ViT/vit.py
@@ -1,10 +1,17 @@
 import torch as t
 
 import matplotlib.pyplot as plt
-
+import json
 import utils
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 class PatchEmbeddings(t.nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.patch_embd = t.nn.Conv2d(in_channels=1)
+        self.patch_embd = t.nn.Conv2d(in_channels=config['num_channels'], 
+                                        out_channels=config['embedding_dim'], stride=config['patch_size'],
+                                        kernel_size=config['patch_size'])
+
+    def forward(self, x:t.Tensor) -> t.Tensor:
+        return t.flatten(self.patch_embd(x), 2).transpose(1,2)
