ea95e0cd3cc224f5f86d83b6e80b91532fb4cd87
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-08-10 11:46:19 +0530

    nn attributes

--- 01_ViT/MHA.py
+++ 01_ViT/MHA.py
@@ -10,3 +10,8 @@ class mha(nn.Module):
         self.num_heads = num_heads
         self.head_dim = dim // num_heads
         self.scale = self.head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, 3*dim, bias=qkv_bias)
+        self.att_d = nn.Dropout(att_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_d = nn.Dropout(proj_drop)
