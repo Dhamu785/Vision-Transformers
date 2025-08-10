f6a4642ee43ba1a35804f5a1c55b54adbdbf3ca9
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-08-10 11:37:32 +0530

    attributes defined

--- 01_ViT/MHA.py
+++ 01_ViT/MHA.py
@@ -1,1 +1,12 @@
 import torch as t
+import torch.nn as nn
+
+class mha(nn.Module):
+    def __init__(self, dim: int, num_heads: int, 
+                    qkv_bias: bool, att_drop: float, 
+                    proj_drop: float) -> None:
+        super().__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
