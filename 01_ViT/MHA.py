a0bfb37a8ec6f81b5725e00efe83f57ae5c59492
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-08-10 12:20:38 +0530

    mha test

--- 01_ViT/MHA.py
+++ 01_ViT/MHA.py
@@ -1,3 +1,4 @@
+# %%
 import torch as t
 import torch.nn as nn
 
@@ -25,7 +26,7 @@ class mha(nn.Module):
         qkv: t.Tensor = qkv.reshape(n_sample, patches, 3, self.num_heads, self.head_dim)
         qkv: t.Tensor = qkv.permute(2, 0, 3, 1, 4) # qkv, n_samples, num_heads, patches, head_dim
         q, k, v = qkv[0], qkv[1], qkv[2]
-        weights: t.Tensor = q @ k.transpose(1,2) # n_samples, num_heads, num_patches, num_patches
+        weights: t.Tensor = q @ k.transpose(-2,-1) # n_samples, num_heads, num_patches, num_patches
         droped_weights: t.Tensor = self.att_d(weights.softmax(-1))
         weighted_value: t.Tensor = droped_weights @ v # n_samples, num_heads, num_patches, head_dim
         v_t: t.Tensor = weighted_value.transpose(1,2) # n_samples, num_patches, num_heads, head_dim
@@ -33,3 +34,11 @@ class mha(nn.Module):
         final_proj: t.Tensor = self.proj_d(self.proj(v_flatten))
 
         return x
+
+# %%
+
+if __name__ == '__main__':
+    x: t.Tensor = t.randn(8, 16, 24)
+    mha_c: mha = mha(24, 6, True, 0.2, 0.2)
+    y: t.Tensor = mha_c(x)
+    print(y.shape)
