330e572c384bc95689b188456795d3ef48224683
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-09-18 18:04:57 +0530

    patch merger

--- 02_SWIN/01_From scratch/parts.py
+++ 02_SWIN/01_From scratch/parts.py
@@ -2,6 +2,18 @@ import torch as t
 from torch import nn
 
 class PatchMerge(nn.Module):
-    def __init__(self, in_channel: int, out_channels: int, downscaling_factor: int):
-            super().__init__()
-            self.patch_merge = nn.Unfold()
+    '''
+        input shape = B, C, H, W
+        output shape = B, H, W, C
+    '''
+    def __init__(self, in_channel: int, out_channels: int, downscaling_factor: int, bias:bool = False):
+        super().__init__()
+        self.downscaling_factor = downscaling_factor
+        self.patch_merge = nn.Unfold(kernel_size=downscaling_factor, stride=downscaling_factor, padding=0)
+        self.linear = nn.Linear(in_features=in_channel*downscaling_factor**2, out_features=out_channels, bias=bias)
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        b, c, h, w = x.shape
+        new_h, new_w = h//self.downscaling_factor, w//self.downscaling_factor
+        x = self.patch_merge(x).view(b, self.downscaling_factor, new_h, new_w).permute(0, 2, 3, 1)
+        x = self.linear(x)
