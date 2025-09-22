dde197d133095dbef9a29090bee75ef452122b0b
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-09-22 18:10:45 +0530

    rough works

--- 02_SWIN/01_From scratch/rough_works.py
+++ 02_SWIN/01_From scratch/rough_works.py
@@ -5,8 +5,11 @@ a = t.tensor([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]])
 print("Shape = ", a.shape)
 print("Array = ",a)
 # %% Rolling implementation
-rolled = t.roll(a, shifts=(2,2), dims=(0,1))
-print(rolled)
+rolled1 = t.roll(a, shifts=(2,2), dims=(0,1))
+rolled2 = t.roll(rolled1, shifts=(-2,-2), dims=(0,1))
+
+print(rolled1)
+print(rolled2)
 # %% Mask analysis==================================
 import torch as t
 # %%
@@ -70,22 +73,27 @@ import numpy as np
 # %%
 def get_relative_distances(window_size):
     indices = t.tensor(np.array([[x, y] for x in range(window_size) for y in range(window_size)]))
-    print(indices)
+    # print(indices)
     distances = indices[None, :, :] - indices[:, None, :]
     return distances
 # %%
-distance = get_relative_distances(3)
+distance = get_relative_distances(7)+6
 print(distance)
+print(distance.shape)
+# %%
+print(distance[40])
 # %% Unfold ==================================
 import torch as t
 # %%
-x = t.randn(8,1,80,80)
+x = t.randn(8,3,224,224)
 # %%
-unfold = t.nn.Unfold(kernel_size=2, padding=0, stride=2)
+unfold = t.nn.Unfold(kernel_size=4, padding=0, stride=4)
 un = unfold(x)
 print(un.shape)
 # %%
-y = un.view(8, -1, 40, 40).permute(0, 2, 3, 1)
+y = un.view(8, -1, 56, 56).permute(0, 2, 3, 1)
 # %%
 y.shape
+# %%
+2*7-1
 # %%
