fdef8d2188a91f6875612d47b4353ca0ec36ad01
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-10-13 19:03:53 +0530

    testing of SWIN block

--- 02_SWIN/01_From scratch/test.py
+++ 02_SWIN/01_From scratch/test.py
@@ -1,9 +1,9 @@
 # %%
-from parts import WindowAttention
+from parts import SwinBlock
 import torch as t
 # %%
 x = t.randn(8, 56, 56, 3)
-wa = WindowAttention(dim=3, heads=3, head_dim=4, shifted=True, shift=2, window_size=7, rel_pos=True)
+wa = SwinBlock(dim=3, heads=3, head_dim=4, shifted=True, shift=2, window_size=7, rel_pos=True, mlp_dim=3*4)
 print(x.shape)
 # %%
 y = wa(x)
