5085870061faab3664a54cf0c79fd9080f758648
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-10-13 13:58:06 +0530

    relative position testing

--- 02_SWIN/01_From scratch/rough_works.py
+++ 02_SWIN/01_From scratch/rough_works.py
@@ -1,27 +1,6 @@
-# %% Relative distance==================================
-import torch as t
-import numpy as np
-# %%
-window_size = 7
-indices = t.from_numpy(np.array([[x, y] for x in range(window_size) for y in range(window_size)]))
-distance = (indices[None, :, :] - indices[:, None, :]) + (window_size-1)
-# print(abs_distance)
-print("Minimum value = ", distance.min().item(), "Maximum value = ", distance.max().item(), "Shape = ", distance.shape)
-
 # %% relative position improved =====================================
 import torch as t
 # %%
-window_size = 7
-coo = t.stack(t.meshgrid([t.arange(window_size), t.arange(window_size)], indexing='ij')) #2,7,7
-coo_flatten = t.flatten(coo, 1) # 2, 49
-rel_coo = (coo_flatten[:,:,None] - coo_flatten[:,None,:]).permute(1,2,0).contiguous() # 2, 49, 49 -> 49, 49, 2
-rel_coo1 = rel_coo + window_size - 1
-rel_coo1[:,:,0]*=13
-rel_coo3 = rel_coo1.sum(-1)
-print(rel_coo3.shape)
-# %% testing of relative position ====================
-import torch as t
-
 window_size = 7
 coo = t.flatten(t.stack(t.meshgrid([t.arange(window_size), t.arange(window_size)], indexing='ij')), 1)
 coo_ij = (coo[:,:,None] - coo[:,None,:]).permute(1,2,0).contiguous()
@@ -30,6 +9,8 @@ coo_ij[:,:,0] *= 2*window_size-1
 rel_1d = coo_ij.sum(-1)
 print(rel_1d.shape)
 print(rel_1d.unique())
+print(len(rel_1d.unique()))
+
 # %% Unfold ==================================
 import torch as t
 # %%
