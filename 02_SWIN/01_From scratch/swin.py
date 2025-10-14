7dbfa7e918eaaba216f9db896bc0c754b69cec8e
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-10-14 19:57:50 +0530

    completed with bug

--- 02_SWIN/01_From scratch/swin.py
+++ 02_SWIN/01_From scratch/swin.py
@@ -5,16 +5,16 @@ import torch as t
 from typing import Tuple
 
 class swin_transformer(t.nn.Module):
-    def __init__(self, in_channesl: int, hidden_dim: int, layers:Tuple[int, ...], heads: Tuple[int, ...], num_clas: int,
+    def __init__(self, in_channels: int, hidden_dim: int, layers:Tuple[int, ...], heads: Tuple[int, ...], num_clas: int,
                     head_dim: int, window_size: int, downscaling_factor: Tuple[int,...], relative_position: bool = True):
         super().__init__()
-        self.stage1 = Stages(in_dim=in_channesl, hidden_dim=hidden_dim, layers=layers[0], downscaling_factor=downscaling_factor[0],
+        self.stage1 = Stages(in_dim=in_channels, hidden_dim=hidden_dim, layers=layers[0], downscaling_factor=downscaling_factor[0],
                                 heads=heads[0], head_dim=head_dim, window_size=window_size, rel_pos=relative_position)
-        self.stage2 = Stages(in_channesl=hidden_dim, hidden_dim=hidden_dim*2, layers=layers[1], downscaling_factor=downscaling_factor[1],
+        self.stage2 = Stages(in_dim=hidden_dim, hidden_dim=hidden_dim*2, layers=layers[1], downscaling_factor=downscaling_factor[1],
                                 heads=heads[1], head_dim=head_dim, window_size=window_size, rel_pos=relative_position)
-        self.stage3 = Stages(in_channesl=in_channesl*2, hidden_dim=hidden_dim*4, layers=layers[2], downscaling_factor=downscaling_factor[2],
+        self.stage3 = Stages(in_dim=hidden_dim*2, hidden_dim=hidden_dim*4, layers=layers[2], downscaling_factor=downscaling_factor[2],
                                 heads=heads[2], head_dim=head_dim, window_size=window_size, rel_pos=relative_position)
-        self.stage4 = Stages(in_channesl=in_channesl*4, hidden_dim=hidden_dim*8, layers=layers[3], downscaling_factor=downscaling_factor[3],
+        self.stage4 = Stages(in_dim=hidden_dim*4, hidden_dim=hidden_dim*8, layers=layers[3], downscaling_factor=downscaling_factor[3],
                                 heads=heads[3], head_dim=head_dim, window_size=window_size, rel_pos=relative_position)
         self.mlp = t.nn.Sequential(
             t.nn.AdaptiveAvgPool2d(output_size=1),
--- 02_SWIN/01_From scratch/test.py
+++ 02_SWIN/01_From scratch/test.py
@@ -1,9 +1,10 @@
 # %%
-from parts import Stages
+from swin import swin_transformer
 import torch as t
 # %%
 x = t.randn(8, 3, 224, 224)
-wa = Stages(in_dim=3, hidden_dim=96, layers=2, downscaling_factor=4, heads=3, head_dim=4, window_size=7, rel_pos=True)
+wa = swin_transformer(in_channels=3, hidden_dim=96, layers=[2,2,6,2], downscaling_factor=[4,2,2,2], heads=(3,6,12,24), 
+                        head_dim=32, window_size=7, relative_position=True, num_clas=10)
 print(x.shape)
 # %%
 y = wa(x)
