ba3f46762eb7882d3814c366f2fefe01fb4b2175
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-10-15 14:21:30 +0530

    Config file added

--- 02_SWIN/01_From scratch/config.py
+++ 02_SWIN/01_From scratch/config.py
@@ -1,2 +1,12 @@
+from typing import Tuple
 class Config:
     batch_size = 8
+    in_channels: int = 3
+    hidden_dim: int = 96
+    layers: Tuple[int, ...] = (2,2,6,2)
+    downscaling_factor: Tuple[int, ...] = (4,2,2,2)
+    heads: Tuple[int, ...] = (3,6,12,24)
+    head_dim: int = 32
+    window_size: int = 7
+    relative_pos: bool = True
+    num_class: int = 10
--- 02_SWIN/01_From scratch/test.py
+++ 02_SWIN/01_From scratch/test.py
@@ -1,11 +1,14 @@
 # %%
 from swin import swin_transformer
 import torch as t
+from config import Config
 DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
 # %%
 x = t.randn(8, 3, 224, 224, device=DEVICE)
-wa = swin_transformer(in_channels=3, hidden_dim=96, layers=[2,2,6,2], downscaling_factor=[4,2,2,2], heads=(3,6,12,24), 
-                        head_dim=32, window_size=7, relative_position=True, num_clas=10).to(DEVICE)
+wa = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
+                        downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
+                        head_dim=Config.head_dim, window_size=Config.window_size, 
+                        relative_position=Config.relative_pos, num_clas=Config.relative_pos).to(DEVICE)
 print(x.shape)
 # %%
 y = wa(x)
