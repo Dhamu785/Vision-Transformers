1eb3414eb06e4aabc56e40cff7e2429f696aa6f0
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-10-30 16:36:51 +0530

    input error in num_class

--- 02_SWIN/01_From scratch/test.py
+++ 02_SWIN/01_From scratch/test.py
@@ -8,7 +8,7 @@ x = t.randn(8, 3, 224, 224, device=DEVICE)
 wa = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
                         downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
                         head_dim=Config.head_dim, window_size=Config.window_size, 
-                        relative_position=Config.relative_pos, num_clas=Config.relative_pos).to(DEVICE)
+                        relative_position=Config.relative_pos, num_clas=Config.num_class).to(DEVICE)
 print(x.shape)
 # %%
 y = wa(x)
