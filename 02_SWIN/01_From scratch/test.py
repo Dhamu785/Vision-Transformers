cc782f8b1e9e7116623f0e955637200bf7085379
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-11-24 15:42:25 +0530

    models layer test

--- 02_SWIN/01_From scratch/test.py
+++ 02_SWIN/01_From scratch/test.py
@@ -5,13 +5,12 @@ from config import Config
 DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
 # %%
 x = t.randn(8, 3, 224, 224, device=DEVICE)
-wa = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
+model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
                         downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
                         head_dim=Config.head_dim, window_size=Config.window_size, 
                         relative_position=Config.relative_pos, num_clas=Config.num_class).to(DEVICE)
-print(x.shape)
 # %%
-y = wa(x)
+model
 # %%
-y.shape
+model.stage1.layers[0][0].mlp
 # %%
