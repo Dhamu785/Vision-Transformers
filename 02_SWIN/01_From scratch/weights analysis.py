f02a0141aea0e434c21a8504d1672a1cda2c5791
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-01-11 22:54:34 +0530

    legend loc

--- 02_SWIN/01_From scratch/weights analysis.py
+++ 02_SWIN/01_From scratch/weights analysis.py
@@ -11,28 +11,27 @@ from config import Config
 # %% load model
 DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
 # mdl_pth = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\food\\custom"
-mdl_pth = "/Users/dhamodharan/My-Python/AI-Tutorials/SWIN supportings/Sample models/numbers/custom"
+mdl_pth = "/Users/dhamodharan/My-Python/AI-Tutorials/SWIN supportings/Sample models/food/custom"
 mdls = os.listdir(mdl_pth)
 
 layer1 = dict()
 for m in mdls:
     mdl = os.path.join(mdl_pth, m)
     if 'pt' in mdl:
-        print(mdl)
         model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
                             downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
                             head_dim=Config.head_dim, window_size=Config.window_size, 
-                            relative_position=Config.relative_pos, num_clas=10).to(DEVICE)
+                            relative_position=Config.relative_pos, num_clas=211).to(DEVICE)
         model.load_state_dict(t.load(mdl, map_location = DEVICE, weights_only = True), strict = False)
         layer1[m] = np.ravel(model.stage1.down_scale.linear.weight.to('cpu').detach().numpy())
 # %%
 plt.figure(figsize=(15,5))
 for i in layer1.keys():
     plt.hist(layer1[i], histtype='step', label='E-'+i.split('-')[1].split('.')[0], alpha=0.8)
-plt.legend()
+plt.legend(loc='upper right')
 plt.xlabel('Weights')
 plt.ylabel('Epochs')
-plt.title('Food | Layer-1')
+plt.title('Food101 | Layer-1')
 plt.grid(visible=True, axis='both', which='both', color='#a1a1a1', linestyle='-', linewidth=1, mec='#00cefc')
 plt.show()
 
@@ -59,6 +58,8 @@ model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidde
                         relative_position=Config.relative_pos, num_clas=10).to(DEVICE)
 model.load_state_dict(t.load(mdl_path, map_location = DEVICE, weights_only = True), strict = False)
 # %%
-model.stage1.down_scale.linear.weight.shape
+weights = model.stage1.down_scale.linear.weight
 
+# %%
+print(weights.tolist())
 # %%
