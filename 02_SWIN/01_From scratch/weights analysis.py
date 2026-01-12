4ef8e0a0f1b5afa523f8e13fb302a8bca86236ec
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-01-12 19:21:21 +0530

    plotting the layer-norm

--- 02_SWIN/01_From scratch/weights analysis.py
+++ 02_SWIN/01_From scratch/weights analysis.py
@@ -43,27 +43,50 @@ for i in layer1.keys():
 plt.legend(loc='right')
 plt.show()
 
-# %% Normalization analysis
+# %% Normalization analysis - Loading the model
 layer_norm1 = dict()
-mdl_path = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\numbers\\custom"
+mdl_path = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\food\\custom"
 # mdl_path = "/Users/dhamodharan/My-Python/AI-Tutorials/SWIN supportings/Sample models/numbers/custom"
 models = os.listdir(mdl_path)
-print(models)
-for m in models:
+file_sorted = sorted(models, key=lambda x: int(x.split('-')[-1].split('.')[0]))
+for m in file_sorted:
     if 'pt' in m:
         mdl = os.path.join(mdl_path, m)
         model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
                                 downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
                                 head_dim=Config.head_dim, window_size=Config.window_size, 
-                                relative_position=Config.relative_pos, num_clas=10).to(DEVICE)
+                                relative_position=Config.relative_pos, num_clas=211).to(DEVICE)
         model.load_state_dict(t.load(mdl, map_location = DEVICE, weights_only = True), strict = False)
 
         gamma = model.stage1.layers[0][0].layer_norm1.weight.detach().cpu().tolist()
         beta = model.stage1.layers[0][0].layer_norm1.bias.detach().cpu().tolist()
         layer_norm1[m] = {'gamma': gamma, 'beta':beta}
+
+# %% Arranging the data
+plotting_data_gamma = dict()
+plotting_data_beta = dict()
+
+layers = 96
+for l in range(layers):
+    plotting_data_gamma[l] = []
+    plotting_data_beta[l] = []
+    for f in file_sorted:
+        plotting_data_gamma[l].append(layer_norm1[f]['gamma'][l])
+        plotting_data_beta[l].append(layer_norm1[f]['beta'][l])
+
+# layer_norm1['mdl-10.pt']['gamma'][0]
+# %% Plotting the data
+layer_no = 0
+plt.figure(figsize=(25,15))
+for i in range(1, 13):
+    plt.subplot(3, 4, i)
+    for j in range(layer_no, layer_no+8):
+        plt.plot(range(0,110,10), plotting_data_gamma[layer_no], label=f'ly-{layer_no}')
+        layer_no += 1
+    plt.legend(loc='upper right')
+
+plt.show()
 # %%
-layer_norm1
-# %% Analysis
 path = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\numbers\\custom\\mdl-100.pt"
 model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
                                 downscaling_factor=Config.downscaling_factor, heads=Config.heads, 
@@ -72,6 +95,5 @@ model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidde
 model.load_state_dict(t.load(path, map_location = DEVICE, weights_only = True), strict = False)
 weights = model.stage1.down_scale.linear.weight.detach().cpu().tolist()
 
-# %%
-model.stage1.layers[0][0].layer_norm1.weight.detach().cpu().tolist()
+
 # %%
