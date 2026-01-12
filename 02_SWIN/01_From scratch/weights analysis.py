91a36d143805411dd32f54ea5611d9168b90ed92
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-01-12 16:03:26 +0530

    removed unwanted lines in weight analysis

--- 02_SWIN/01_From scratch/weights analysis.py
+++ 02_SWIN/01_From scratch/weights analysis.py
@@ -7,12 +7,13 @@ import os
 from swin import swin_transformer
 from config import Config
 
-# %% load model
+# %% Giving the path
 DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
 mdl_pth = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\numbers\\custom"
 # mdl_pth = "/Users/dhamodharan/My-Python/AI-Tutorials/SWIN supportings/Sample models/food/custom"
 mdls = os.listdir(mdl_pth)
 
+# %% Load model and plotting layer1
 layer1 = dict()
 for m in mdls:
     mdl = os.path.join(mdl_pth, m)
@@ -23,7 +24,7 @@ for m in mdls:
                             relative_position=Config.relative_pos, num_clas=10).to(DEVICE)
         model.load_state_dict(t.load(mdl, map_location = DEVICE, weights_only = True), strict = False)
         layer1[m] = np.ravel(model.stage1.down_scale.linear.weight.to('cpu').detach().numpy())
-# %%
+
 plt.figure(figsize=(15,5))
 for i in layer1.keys():
     plt.hist(layer1[i], histtype='step', label='E-'+i.split('-')[1].split('.')[0], alpha=0.8)
@@ -34,7 +35,7 @@ plt.title('SVHN | Layer-1')
 plt.grid(visible=True, axis='both', which='both', color='#a1a1a1', linestyle='-', linewidth=1, mec='#00cefc')
 plt.show()
 
-# %% curve
+# %% curve using bin centers
 for i in layer1.keys():
     counts, bins = np.histogram(layer1[i], bins=10)
     bin_centers = (bins[:-1] + bins[1:]) / 2
@@ -42,13 +43,7 @@ for i in layer1.keys():
 plt.legend(loc='right')
 plt.show()
 
-# %%
-from swin import swin_transformer
-from config import Config
-import torch as t
-# %%
-DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
-# %%
+# %% Normalization analysis
 # mdl_path = "C:\\Users\\dhamu\\Downloads\\SWIN\\Sample models\\numbers\\custom\\mdl-100.pt"
 mdl_path = "/Users/dhamodharan/My-Python/AI-Tutorials/SWIN supportings/Sample models/numbers/custom/mdl-10.pt"
 model = swin_transformer(in_channels=Config.in_channels, hidden_dim=Config.hidden_dim, layers=Config.layers, 
